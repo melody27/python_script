c6cecc9e3425d0f60fac728c8ca62024c55fee6a
melody <57315539+melody27@users.noreply.github.com>
2023-09-19 10:49:24 +0800

    1
    1

--- t10.py
+++ t10.py
@@ -1,16 +1,77 @@
+import functools
+
+def _not_divisible(n):
+    return lambda x: x % n > 0
+
+
+def _odd_iter():
+    n = 1
+    while True:
+        n = n + 2
+        yield n
 
 
 
-def com(n):
-    left = 0.0
-    for i in range(n,21):
-        if (i-1 == 0):continue
-        left += (n-1)/(i-1)
-    # return (21-n)/20*left/(21-n)
-    return 1/20*left
+
+def primes():
+    yield 2
+    it = _odd_iter() # 初始序列(奇数队列)
+    while True:
+
+        n = next(it) # 返回序列的第一个数
+        yield n
+        it = filter(_not_divisible(n), it) # 构造新序列。相当于lambda x:x%3
+
+
+
+def log(func):
+    @functools.wraps(func)      # 简单可以理解为将wapper的__name__属性进行替换
+    def wapper(*arg,**kw):
+        print("called :"+func.__name__ + "()")
+        return func(*arg,**kw)
+    return wapper
+
+
+@log
+def now():
+    print("2015-7-10")
+    return 
+
+def __private_test():
+    print("this is a private test")
+
+
+class Stu(object):
+    
+    def __init__(self,name,age):
+        self.__name = name                   # __name属性外部不能直接引用
+        self.__age = age
+        self.__sex__ = 'm'                  # 特殊变量，外部可以使用
+
+    def print_info(self):
+        print(self.__name,self.__age)
 
 
 
 if __name__ == "__main__":
-    for i in range(1,21):
-        print(str(i)+":"+str(com(i)))
+    # for n in primes():
+    #     if n < 1000:
+    #         print(n)
+    #     else:
+    #         break
+    
+    # now()           # 应该参数类型检查是在预编译阶段做的
+    # print(now.__name__)
+    # __private_test()
+
+    d1 = Stu("coco",17) 
+    d1.print_info()
+    print("the sex of d1 is "+d1.__sex__)                   #
+    d1.__name__ = "melody"                # 此处 __name__的self.__name__不是同一个属性
+    print(d1.__name__)
+    d1.print_info()                     # 对于python动态语言，多态的使用只需要有同名方法即可
+    print(hasattr(d1,"type"))
+    print(setattr(d1,"type","man"))     # 通过setattr这种函数，来进行属性的设置
+    print(getattr(d1,"type"))
+    print(getattr(d1,"school","cqu"))   # 第三个参数可以返回默认值
+    
---
+++ task_processing/a1.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+Month = Enum('Month', ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'))
+
+
+
+if __name__ == "__main__":
+    
+
+    for name, member in Month.__members__.items():
+        print(name, '=>', member, ',', member.value)
+
+
+    # print(Month)
